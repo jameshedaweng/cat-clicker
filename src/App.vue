031a5cb922bec9787880cc5aeaa836ba0a16fd49
James Heda Weng <james@Jamess-MacBook-Pro.local>
2018-06-10 18:29:07 +0200

    Show a basic cat image in a component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
     <h1>Cat Clicker</h1>
+    <div id="main-container">
+      <CatContainer></CatContainer>
+    </div>
   </div>
 </template>
 
 <script>
+  import CatContainer from './components/CatContainer';
 
-export default {
-  name: 'app',
-  components: {
-    
+  export default {
+    name: 'app',
+    components: {
+      CatContainer
+    }
   }
-}
 </script>
 
 <style lang="scss">
---
+++ src/components/CatContainer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="cat-container">
+    <img class="cat-image" src="../assets/images/normal-cat.jpg">
+  </div>
+</template>
+
+<script>
+  name: 'cat-container'
+</script>
+
+<style lang="scss" scoped>
+  @import '../assets/scss/pages/cat_container';
+</style>
