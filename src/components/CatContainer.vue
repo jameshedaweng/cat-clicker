4b1c8040b333cd5586f069f702cca45fa641120e
James Heda Weng <james@Jamess-MacBook-Pro.local>
2018-06-10 18:34:12 +0200

    Add a counter when click on the CatContainer component

--- src/components/CatContainer.vue
+++ src/components/CatContainer.vue
@@ -1,11 +1,19 @@
 <template>
-  <div class="cat-container">
+  <div class="cat-container" v-on:click="counter += 1">
     <img class="cat-image" src="../assets/images/normal-cat.jpg">
+    <span>You clicked on this cat {{ counter }} times.</span>
   </div>
 </template>
 
 <script>
-  name: 'cat-container'
+  export default {
+    name: 'cat-container',
+    data: function() {
+      return {
+        counter: 0
+      };
+    }
+  };
 </script>
 
 <style lang="scss" scoped>
