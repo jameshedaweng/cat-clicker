05c77bb241218c68d3789f4b35521b17ec44a7c8
James Heda Weng <james@Jamess-MacBook-Pro.local>
2018-06-10 19:55:08 +0200

    Make 2 cat containers, each with its image, name and counter

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,28 @@
   <div id="app">
     <h1>Cat Clicker</h1>
     <div id="main-container">
-      <CatContainer></CatContainer>
+      <CatContainer
+        v-for="(cat, index) in cats" 
+        v-bind:key="`cat-${index}`"
+        v-bind:cat="cat"
+      >
+      </CatContainer>
     </div>
   </div>
 </template>
 
 <script>
   import CatContainer from './components/CatContainer';
-
+  import cats from './assets/static/cats.json';
   export default {
     name: 'app',
     components: {
       CatContainer
+    },
+    data: function() {
+      return {
+        cats: cats
+      }
     }
   }
 </script>
--- src/components/CatContainer.vue
+++ src/components/CatContainer.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="cat-container" v-on:click="counter += 1">
-    <img class="cat-image" src="../assets/images/normal-cat.jpg">
-    <span>You clicked on this cat {{ counter }} times.</span>
+    <img class="cat-image" v-bind:src="imagePath(cat.file)" v-bind:alt="cat.name">
+    <h2>{{ cat.name }}</h2>
+    <div class="cat-info">
+      <span class="cat-count">{{ counter }}</span> clicks on this cat
+    </div>
   </div>
 </template>
 
 <script>
   export default {
     name: 'cat-container',
+    props: ['cat'],
     data: function() {
       return {
         counter: 0
       };
+    },
+    methods: {
+      imagePath(image) {
+        return require(`../assets/images/${image}`);
+      }
     }
   };
 </script>
